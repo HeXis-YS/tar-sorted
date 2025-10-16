4f22231f1aa6f8e3a3f63d35f54c571308376987
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-10-16 20:47:37 +0800

    Compute hash only when sizes equal

--- tar-sorted.py
+++ tar-sorted.py
@@ -13,11 +13,13 @@ def read_md5(path):
 
 class Tree:
     def __init__(self, mode="print", file=sys.stdout, verbose=False):
-        self.files = {} # md5: [(dirname, basename, ext, md5, path)]
         self.mode = mode
         self.file = file
         self.verbose = verbose
         assert self.mode in ("print", "print0")
+        # Collect entries; md5 will be computed later only for same-size files
+        # entry tuple: (dirname, basename, ext, size, is_reg, path)
+        self.entries = []
 
     def emit(self, path):
         self.file.write(path)
@@ -32,27 +34,46 @@ class Tree:
         else:
             dirname, basename = os.path.split(path)
             ext = os.path.splitext(basename)[1].lower()
-            if stat.S_ISREG(st.st_mode):
-                md5 = read_md5(path)
-            else:
-                md5 = None
-            self.files.setdefault(md5, []).append(
-                (dirname, basename, ext, md5, path))
+            is_reg = stat.S_ISREG(st.st_mode)
+            size = st.st_size if is_reg else None
+            self.entries.append((dirname, basename, ext, size, is_reg, path))
 
     def process(self):
         def sort_key(x):
             return x[2], x[1], x[0] # ext, basename, dirname
 
-        # Sort by extension, but emit all files with the same MD5 together
-        md5s = set()
-        for _, _, _, md5, _ in sorted(
-                (y for x in self.files.values() for y in x), key=sort_key):
-            if md5 not in md5s:
-                md5s.add(md5)
-                # emit all paths with the same md5
-                for path in sorted(
-                        (path for _, _, _, _, path in self.files[md5])):
-                    self.emit(path)
+        # Group regular files by size so we only hash potential duplicates
+        by_size = {}
+        for dirname, basename, ext, size, is_reg, path in self.entries:
+            if is_reg:
+                by_size.setdefault(size, []).append((dirname, basename, ext, size, is_reg, path))
+
+        # Assign md5 keys:
+        # - regular files with a unique size -> unique sentinel per path (no hashing)
+        # - regular files with duplicate size -> compute real md5
+        # - non-regular files -> md5 key = None (same as original behavior)
+        by_md5 = {}
+        records = []  # (dirname, basename, ext, md5_key, path)
+
+        for dirname, basename, ext, size, is_reg, path in self.entries:
+            if not is_reg:
+                md5_key = None
+            else:
+                group = by_size.get(size, [])
+                if len(group) <= 1:
+                    md5_key = ("solo", path)  # unique sentinel, avoids hashing
+                else:
+                    md5_key = read_md5(path)
+            by_md5.setdefault(md5_key, []).append(path)
+            records.append((dirname, basename, ext, md5_key, path))
+
+        # Sort by ext/basename/dirname, then emit first time we see each md5_key
+        seen = set()
+        for _, _, _, md5_key, _ in sorted(records, key=sort_key):
+            if md5_key not in seen:
+                seen.add(md5_key)
+                for p in sorted(by_md5[md5_key]):
+                    self.emit(p)
 
     def close(self):
         pass
